09703b576eeb11f8d5fd52e165c71b5c172c1ff7
Nathan Mathis <ntmathis@maine.rr.com>
2017-12-04 00:39:21 -0500

    updated django-cc to support django 2.0

--- crypto_bear_bucks/wallet/admin.py
+++ crypto_bear_bucks/wallet/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django_cc.cc.models import Wallet, Currency
+
+class WalletAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Wallet, WalletAdmin)
